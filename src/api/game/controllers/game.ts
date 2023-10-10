dda0b2ead44868baf922dfb45962d1c610added4
Marcos Domingues <marcos.domingues@poncetech.com.br>
2023-10-10 11:22:51 -0300

    (config) post games

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -1,7 +1,19 @@
 /**
  * game controller
  */
+import { factories } from "@strapi/strapi";
+export default factories.createCoreController(
+  "api::game.game",
+  ({ strapi }) => ({
+    async populate(ctx) {
+      const options = {
+        limit: 48,
+        order: "desc:trending",
+        ...ctx.query,
+      };
 
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::game.game');
+      await strapi.service("api::game.game").populate(options);
+      ctx.send("Finished populating games!");
+    },
+  })
+);
